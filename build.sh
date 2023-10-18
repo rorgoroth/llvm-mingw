73dac4e5eb31cf89b8ef689806fbaea313f7e989
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2023-10-18 16:07:46 +0100

    17.0.3

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ rm -rf *.tar.xz
 rm -v llvm-x86_64-w64-mingw32/x86_64-w64-mingw32/lib/*.dll.a
 cp -v llvm-x86_64-w64-mingw32/x86_64-w64-mingw32/lib/libc++.a llvm-x86_64-w64-mingw32/x86_64-w64-mingw32/lib/libstdc++.a
 
-apack llvm-x86_64-w64-mingw32_17.0.1.tar.xz llvm-x86_64-w64-mingw32
+apack llvm-x86_64-w64-mingw32_17.0.3.tar.xz llvm-x86_64-w64-mingw32
