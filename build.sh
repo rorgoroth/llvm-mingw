c53fbd44687d27a926010be3094eb1e1493ee1d7
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-02-18 21:00:19 +0000

    17.0.6-r4

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ git clean -xdf
                --disable-cfguard \
                --with-default-msvcrt=ucrt \
                --with-default-win32-winnt=0x0A00 \
+               --thinlto \
                llvm-x86_64-w64-mingw32
 
 cp -v llvm-x86_64-w64-mingw32/x86_64-w64-mingw32/lib/libc++.a \
       llvm-x86_64-w64-mingw32/x86_64-w64-mingw32/lib/libstdc++.a
 
-apack 17.0.6-r3.tar.xz llvm-x86_64-w64-mingw32
+apack 17.0.6-r4.tar.xz llvm-x86_64-w64-mingw32
